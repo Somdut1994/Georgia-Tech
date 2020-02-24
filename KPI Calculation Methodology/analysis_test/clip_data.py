0bde55d28b3f4c24caabe7d4bcd8b2a488936631
Somdut1994 <60203338+Somdut1994@users.noreply.github.com>
2020-02-23 19:40:27 -0500

    Delete clip_data.py

--- KPI Calculation Methodology/analysis_test/clip_data.py
+++
@@ -1,7 +0,0 @@
-with open('Sample_Trajectory.fzp', 'w+') as outfile:
-	with open('Peak_PM.txt') as infile:
-		for line in infile:
-			f=line.split('\n')[0].split(';')
-			if float(f[0])==57720.0:
-				break
-			outfile.write(line)
